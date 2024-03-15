c34f301ea3d90520c551a23457ad65555618493f
Manit <manit.manu4@gmail.com>
2024-03-16 00:01:11 +0530

    second_15-03

--- react_src/src/components/AllProducts.js
+++ react_src/src/components/AllProducts.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from './Product'
+import axios from 'axios'
+import { Button } from 'reactstrap'
 
 const AllProducts= ()=> {
 
-    var products = [
-        {
-            pid:100,
-            name:'Sony Led',
-            price:400
+    useEffect(() => {
+       alert('testing...')
+       getAllProducts();
+    }, [])
+
+    // var products = [
+    //     {
+    //         pid:100,
+    //         name:'Sony Led',
+    //         price:400
+    //     },
+    //     {
+    //         pid:101,
+    //         name:'Samsung Led',
+    //         price:300
+    //     } 
+    // ]
+
+
+    var [products, setProducts] = useState([])
+
+    function getAllProducts() {
+
+        axios.get('http://localhost:9898/allProducts').then((response) => {
+            console.log('resposne is ',response)
+            products = setProducts(response.data);
         },
-        {
-            pid:101,
-            name:'Samsung Led',
-            price:300
-        } 
-    ]
+        (error) => {
+            console.log('error is ',error);
+        })
+    }
 
     return (
         <div>
-
             {
                 products.map((item) => <Product product = {item}/>)
             }
